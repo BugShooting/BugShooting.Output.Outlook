a275bb275e0974da1d86e5d31d657a4ae6318279
Alexej Hirsch <alexej@bugshooting.com>
2017-07-11 16:36:06 +0200

    Projektdateien hinzufügen.

---
+++ BS.Output.Outlook/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BS.Output.Outlook")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bug Shooting")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0776A310-CFB1-40C4-BB68-B3278419241B")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
